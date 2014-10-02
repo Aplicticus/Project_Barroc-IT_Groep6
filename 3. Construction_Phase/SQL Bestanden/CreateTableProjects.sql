9ed3226c3df18321e6a22897284342a1ebf21c91
markieo1 <marcohavermans@hotmail.nl>
2014-10-02 09:36:12 +0200

    sql bestande toegevoegd

---
+++ 3. Construction_Phase/SQL Bestanden/CreateTableProjects.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE tbl_Projects
+(
+	ProjectID INT IDENTITY(1,1) PRIMARY KEY,
+	CustomerID INT NOT NULL,
+	Name varchar(30) NOT NULL,
+	Deadline DATE NOT NULL,
+	Subject varchar(30) NOT NULL
+)
