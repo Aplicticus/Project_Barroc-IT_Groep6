c7b8aabddb7b135f5d219fae4726274fe727cf5c
markieo1 <marcohavermans@hotmail.nl>
2014-10-03 14:19:35 +0200

    Database en handler toegevoegd

---
+++ 3. Construction_Phase/Application/Barroc-IT/Barroc-IT/DatabaseHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Barroc_IT
+{
+    class DatabaseHandler
+    {
+        private static SqlConnection conn;
+
+        public DatabaseHandler()
+        {
+            conn = new SqlConnection();
+            conn.ConnectionString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\Resources\BarrocIT.mdf;Integrated Security=True";
+        }
+        public DatabaseHandler(string connectionString)
+        {
+            conn = new SqlConnection();
+            conn.ConnectionString = connectionString;
+        }
+        public void TestConnection()
+        {
+            try
+            {
+                conn.Open();
+                MessageBox.Show("conn opE1!");
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
