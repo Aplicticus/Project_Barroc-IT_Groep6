ffa34925d4a3dc528b666ac4d9ab6720606eb476
markieo1 <marcohavermans@hotmail.nl>
2014-10-15 13:46:44 +0200

    commit

--- 3. Construction_Phase/Application/Barroc-IT/Barroc-IT/frmLogin.cs
+++ 3. Construction_Phase/Application/Barroc-IT/Barroc-IT/frmLogin.cs
@@ -15,7 +15,7 @@ namespace Barroc_IT
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void frmLogin_Load(object sender, EventArgs e)
         {
             handler = new DatabaseHandler();            
         }
